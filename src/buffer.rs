ead8cb05c08230d29d02b9bcb872e4a9351b7cf7
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-05-09 22:49:03 +0900

    add: BufferPool

---
+++ src/buffer.rs
@@ -0,0 +1,42 @@
+use std::cell::{Cell, RefCell};
+use std::rc::Rc;
+
+use crate::disk::{DiskManager, PageId, PAGE_SIZE};
+
+pub struct BufferId(usize);
+
+pub type Page = [u8; PAGE_SIZE];
+
+pub struct Buffer {
+    pub page_id: PageId,
+    pub page: RefCell<Page>,
+    pub is_dirty: Cell<bool>,
+}
+
+pub struct Frame {
+    usage_count: u64,
+    buffer: Rc<Buffer>,
+}
+
+pub struct BufferPool {
+    buffers: Vec<Frame>,
+    next_victim_id: BufferId,
+}
+
+impl Buffer {
+    pub fn check_call() {
+        println!("from Buffer");
+    }
+}
+
+impl Frame {
+    pub fn check_call() {
+        println!("from Frame");
+    }
+}
+
+impl BufferPool {
+    pub fn check_call() {
+        println!("from BufferPool");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod disk;
+mod buffer;
 
 fn main() {
     disk::DiskManager::check_call();
+    buffer::Buffer::check_call();
+    buffer::Frame::check_call();
+    buffer::BufferPool::check_call();
 
     println!("Hello, world!");
 }
